82aca8fcbdbfa44eed54ec7606bec9b8d3fa4171
williford-abunas <abunaswilliford@gmail.com>
2024-03-24 00:05:33 +1300

    components

--- pizza-menu/src/index.tsx
+++ pizza-menu/src/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
 import ReactDOM, { Container } from 'react-dom/client'
+import './index.css'
+import { PizzaModel, pizzaData } from '../public/data'
 
 function App() {
-  return <h1>Hello React!</h1>
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  )
+}
+
+function Header() {
+  return (
+    <>
+      <h1 className="text-red">Fast React Pizza Co.</h1>
+    </>
+  )
+}
+function Menu() {
+  return (
+    <>
+      <h2>Our Menu</h2>
+      <Pizza />
+    </>
+  )
+}
+function Footer() {
+  const hour = new Date().getHours()
+  const openHour = 12
+  const closeHour = 24
+  const isOpen = hour >= openHour && hour <= closeHour
+  console.log(isOpen)
+
+  return (
+    <>
+      <footer>{new Date().toLocaleTimeString()} - We're currently open!</footer>
+    </>
+  )
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza</h2>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Container)
