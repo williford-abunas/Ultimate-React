2e5e6ecbc7d1692e52d61151473baaa557994610
williford-abunas <abunaswilliford@gmail.com>
2024-03-26 02:38:09 +1300

    controlled forms

---
+++ travel-list/src/App.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+]
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
+}
+
+function Logo() {
+  return <h1>🚢 Far Away 🧳</h1>
+}
+
+function Form() {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!description) return
+
+    const newItem: itemModel = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    }
+
+    setDescription('')
+    setQuantity(1)
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  )
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+interface itemModel {
+  id: number
+  description: string
+  quantity: number
+  packed: boolean
+}
+
+interface itemProp {
+  item: itemModel
+}
+
+function Item({ item }: itemProp) {
+  return (
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  )
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list.</em>
+    </footer>
+  )
+}
