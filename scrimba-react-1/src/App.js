4e4ed3beaf0acd30d8671db7a37f5d13421b8f6a
williford-abunas <abunaswilliford@gmail.com>
2024-04-13 20:33:31 +1200

    static react page

--- scrimba-react-1/src/App.js
+++ scrimba-react-1/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <Main />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+const Main = () => {
+  return (
+    <div className="main-container">
+      <h1>Fun facts about React:</h1>
+      <ul>
+        <li>Was first realeased in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navEl">
+        <img src={logo} alt="logo" className="logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>developed by Pocodev 2024</p>
+    </footer>
+  )
+}
+
+export default App
