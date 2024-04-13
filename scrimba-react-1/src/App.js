a344c904412acf58e1685adcb118e0852142dd48
williford-abunas <abunaswilliford@gmail.com>
2024-04-14 00:52:26 +1200

    style react static page

--- scrimba-react-1/src/App.js
+++ scrimba-react-1/src/App.js
@@ -1,50 +1,12 @@
-import logo from './logo.svg'
+import Navbar from './components/Navbar.tsx'
+import Main from './components/Main.tsx'
 
-function App() {
+const App = () => {
   return (
-    <div className="app-container">
-      <Header />
+    <>
+      <Navbar />
       <Main />
-      <Footer />
-    </div>
-  )
-}
-
-const Main = () => {
-  return (
-    <div className="main-container">
-      <h1>Fun facts about React:</h1>
-      <ul>
-        <li>Was first realeased in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  )
-}
-
-const Header = () => {
-  return (
-    <header>
-      <nav className="navEl">
-        <img src={logo} alt="logo" className="logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
-
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>developed by Pocodev 2024</p>
-    </footer>
+    </>
   )
 }
 
---
+++ scrimba-react-1/src/components/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main className="main-container">
+      <div className="main-content">
+        <h1>Fun facts about React</h1>
+        <ul className="list-items">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+export default Main
