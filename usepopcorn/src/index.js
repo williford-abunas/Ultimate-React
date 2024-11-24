6554493e170dced5d8ac18efdf9c67d3712ef01f
williford-abunas <abunaswilliford@gmail.com>
2024-11-24 15:23:38 +1300

    setup starter

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css';
-// import App from './App.tsx';
+import './index.css';
+import App from './App.tsx';
 
-import StarRating from './StarRating.tsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating
-      maxRating={5}
-      color={'blue'}
-      size={24}
-      messages={['first', 'second', 'third', 'fourth', 'fifth']}
-      defaultRating={3}
-    />
+    <App />
   </React.StrictMode>
 )
