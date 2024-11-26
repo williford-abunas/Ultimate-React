38781eb68e5518d06ce9e0d6e38af01e1d5e0b55
williford-abunas <abunaswilliford@gmail.com>
2024-11-26 16:28:44 +1300

    done reusable starRating component

--- 17-usepopcorn/src/index.js
+++ 17-usepopcorn/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
 import StarRating from "./StarRating.js"
+import {useState} from 'react'
+
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return <div>
+    <StarRating maxRating={10} color="blue" onSetRating = {setMovieRating}/>
+    <p>Rating is {movieRating}</p>
+  </div>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating messages = {[ "Terrible", 'Okay', 'Good', 'Very Good', "Amazing"]}/>
+    <StarRating color="red" size={25} defaultRating={3}/>
+    <Test />
   </React.StrictMode>
 );
 
