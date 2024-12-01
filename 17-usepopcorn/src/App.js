24de0f9608f8dd0a23e9f7c41eb975d8f9022fe6
williford-abunas <abunaswilliford@gmail.com>
2024-12-01 15:12:54 +1300

    installed dotenv

--- 17-usepopcorn/src/App.js
+++ 17-usepopcorn/src/App.js
@@ -7,28 +7,27 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
 
   return (
     <>
-    <Navbar>
-    <Search />
-    <NumResults movies={movies}/>
-    </Navbar>
+      <Navbar>
+        <Search />
+        <NumResults movies={movies} />
+      </Navbar>
       <Main>
-      <Box>
-      <MovieList movies={movies}/>
-      </Box>
-      <Box>
-      <WatchedSummary watched={watched}/>
-      <WatchedMoviesList watched={watched}/>
-      </Box>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
 }
 
-function Navbar({children}) {
+function Navbar({ children }) {
 
   return <nav className="nav-bar">
     <Logo />
@@ -39,129 +38,129 @@ function Navbar({children}) {
 
 function Logo() {
   return <div className="logo">
-  <span role="img">🍿</span>
-  <h1>usePopcorn</h1>
-</div>
+    <span role="img">🍿</span>
+    <h1>usePopcorn</h1>
+  </div>
 }
 
 function Search() {
   const [query, setQuery] = useState("");
 
- return <input
-      className="search"
-      type="text"
-      placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
+  return <input
+    className="search"
+    type="text"
+    placeholder="Search movies..."
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+  />
 }
 
-function NumResults({movies}) {
+function NumResults({ movies }) {
   return <p className="num-results">
-  Found <strong>{movies.length}</strong> results
-</p>
+    Found <strong>{movies.length}</strong> results
+  </p>
 }
 
-function Main({children}) {
-  
-  
+function Main({ children }) {
+
+
   return <main className="main">
-  {children}
-</main>
+    {children}
+  </main>
 }
 
 
-function Box({children}) {
-const [isOpen, setIsOpen] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
   return <div className="box">
-  <button
-    className="btn-toggle"
-    onClick={() => setIsOpen((open) => !open)}
-  >
-    {isOpen ? "–" : "+"}
-  </button>
-  {isOpen && 
-    children
-  }
-</div>
+    <button
+      className="btn-toggle"
+      onClick={() => setIsOpen((open) => !open)}
+    >
+      {isOpen ? "–" : "+"}
+    </button>
+    {isOpen &&
+      children
+    }
+  </div>
 }
 
-function MovieList({movies}) {
+function MovieList({ movies }) {
 
   return <ul className="list">
-  {movies?.map((movie) => (
-    <Movie movie={movie} key={movie.imdbID}/>
-  ))}
-</ul>
+    {movies?.map((movie) => (
+      <Movie movie={movie} key={movie.imdbID} />
+    ))}
+  </ul>
 }
 
-function Movie({movie}) {
+function Movie({ movie }) {
   return <li >
-  <img src={movie.Poster} alt={`${movie.Title} poster`} />
-  <h3>{movie.Title}</h3>
-  <div>
-    <p>
-      <span>🗓</span>
-      <span>{movie.Year}</span>
-    </p>
-  </div>
-</li>
+    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <h3>{movie.Title}</h3>
+    <div>
+      <p>
+        <span>🗓</span>
+        <span>{movie.Year}</span>
+      </p>
+    </div>
+  </li>
 }
 
 
-function WatchedSummary({watched}) {
+function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
   return <div className="summary">
-  <h2>Movies you watched</h2>
-  <div>
-    <p>
-      <span>#️⃣</span>
-      <span>{watched.length} movies</span>
-    </p>
-    <p>
-      <span>⭐️</span>
-      <span>{avgImdbRating}</span>
-    </p>
-    <p>
-      <span>🌟</span>
-      <span>{avgUserRating}</span>
-    </p>
-    <p>
-      <span>⏳</span>
-      <span>{avgRuntime} min</span>
-    </p>
+    <h2>Movies you watched</h2>
+    <div>
+      <p>
+        <span>#️⃣</span>
+        <span>{watched.length} movies</span>
+      </p>
+      <p>
+        <span>⭐️</span>
+        <span>{avgImdbRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{avgUserRating}</span>
+      </p>
+      <p>
+        <span>⏳</span>
+        <span>{avgRuntime} min</span>
+      </p>
+    </div>
   </div>
-</div>
 }
 
-function WatchedMoviesList({watched}) {
+function WatchedMoviesList({ watched }) {
   return <ul className="list">
-  {watched.map((movie) => (
-      <WatchedMovie movie={movie} key={movie.imdbID}/>
-  ))}
-</ul>
+    {watched.map((movie) => (
+      <WatchedMovie movie={movie} key={movie.imdbID} />
+    ))}
+  </ul>
 }
 
-function WatchedMovie({movie}) {
-return <li >
-<img src={movie.Poster} alt={`${movie.Title} poster`} />
-<h3>{movie.Title}</h3>
-<div>
-  <p>
-    <span>⭐️</span>
-    <span>{movie.imdbRating}</span>
-  </p>
-  <p>
-    <span>🌟</span>
-    <span>{movie.userRating}</span>
-  </p>
-  <p>
-    <span>⏳</span>
-    <span>{movie.runtime} min</span>
-  </p>
-</div>
-</li>
+function WatchedMovie({ movie }) {
+  return <li >
+    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <h3>{movie.Title}</h3>
+    <div>
+      <p>
+        <span>⭐️</span>
+        <span>{movie.imdbRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{movie.userRating}</span>
+      </p>
+      <p>
+        <span>⏳</span>
+        <span>{movie.runtime} min</span>
+      </p>
+    </div>
+  </li>
 }
 
--- 17-usepopcorn/src/index.js
+++ 17-usepopcorn/src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
-import StarRating from "./StarRating.js"
-import {useState} from 'react'
+import './index.css';
+import App from './App';
+// import StarRating from "./StarRating.js"
+// import { useState } from 'react'
 
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0)
-  return <div>
-    <StarRating maxRating={10} color="blue" onSetRating = {setMovieRating}/>
-    <p>Rating is {movieRating}</p>
-  </div>
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0)
+//   return <div>
+//     <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+//     <p>Rating is {movieRating}</p>
+//   </div>
+// }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating messages = {[ "Terrible", 'Okay', 'Good', 'Very Good', "Amazing"]}/>
+    <App />
+    {/* <StarRating messages = {[ "Terrible", 'Okay', 'Good', 'Very Good', "Amazing"]}/>
     <StarRating color="red" size={25} defaultRating={3}/>
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
 
