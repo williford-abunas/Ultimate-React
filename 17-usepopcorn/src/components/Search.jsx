fc1fe49ada2f266938d41d401bee1704a0f2cbc4
williford-abunas <abunaswilliford@gmail.com>
2024-12-07 01:25:17 +1300

    useRef to focus inputEl

--- 17-usepopcorn/src/components/Search.jsx
+++ 17-usepopcorn/src/components/Search.jsx
@@ -1,10 +1,29 @@
-export default function Search({ query, setQuery }) {
+import { useEffect, useRef } from 'react'
 
-  return <input
-    className="search"
-    type="text"
-    placeholder="Search movies..."
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-  />
+export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null)
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current)
+        return
+      if (e.code === 'Enter') {
+        inputEl.current.focus()
+        setQuery("")
+      }
+    }
+    document.addEventListener('keydown', callback)
+    return () => document.removeEventListener('keydown', callback)
+  }, [setQuery])
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
+  )
 }
