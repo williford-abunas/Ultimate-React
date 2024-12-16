fba2a044205987a773fca473f3cc0fd6c6c0185a
williford-abunas <abunaswilliford@gmail.com>
2024-12-17 00:46:37 +1300

    implemented Route and Navlink

---
+++ 21-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import PageNav from '../components/PageNav'
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ 21-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav'
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>Not Found!</p>
+    </div>
+  )
+}
+
+export default PageNotFound
