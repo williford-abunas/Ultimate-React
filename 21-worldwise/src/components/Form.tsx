a503ab6657c353a4c4025ee4e89d87e6c5b32c08
williford-abunas <abunaswilliford@gmail.com>
2025-03-02 10:57:24 +1300

    removed controller

--- 21-worldwise/src/components/Form.tsx
+++ 21-worldwise/src/components/Form.tsx
@@ -1,64 +1,62 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from "react";
-import {useUrlPosition} from '../hooks/useUrlPosition.ts'
-import styles from "./Form.module.css";
+import { useState, useEffect } from 'react'
+import { useUrlPosition } from '../hooks/useUrlPosition.ts'
+import styles from './Form.module.css'
 import BackButton from './BackButton.tsx'
 import Button from './Button.tsx'
 import Message from './Message.tsx'
 import Spinner from './Spinner.tsx'
 
-
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
+    .split('')
+    .map((char) => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
 }
 
-const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client"
+const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client'
 
 function Form() {
   const [mapLat, mapLng] = useUrlPosition()
   const navigate = useNavigate()
-  const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
-  const [date, setDate] = useState(new Date());
-  const [notes, setNotes] = useState("");
+  const [cityName, setCityName] = useState('')
+  const [country, setCountry] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [notes, setNotes] = useState('')
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false)
-  const [emoji, setEmoji] = useState("")
-  const [geocodingError, setGeocodingError] = useState("")
+  const [emoji, setEmoji] = useState('')
+  const [geocodingError, setGeocodingError] = useState('')
 
   useEffect(() => {
     if (mapLat === null || mapLng === null) return
-    
-    const controller = new AbortController()
-    const signal = controller.signal
 
-      async function fetchCityData() {
-        try {
-          setIsLoadingGeocoding(true)
-          setGeocodingError("")
-          const res = await fetch(`${BASE_URL}?latitude=${mapLat}&longitude=${mapLng}`, {signal})
-          const data = await res.json()
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true)
+        setGeocodingError('')
+        const res = await fetch(
+          `${BASE_URL}?latitude=${mapLat}&longitude=${mapLng}`
+        )
+        const data = await res.json()
 
-          if(!data.countryCode) throw new Error("Not a city. Click Somewhere else...")
-          setCityName(data.city || data.locality || "")
-          setCountry(data.countryName)
-          setEmoji(convertToEmoji(data.countryCode))
-        } catch (error) {
-          setGeocodingError(error.message)
-        } finally {
-          setIsLoadingGeocoding(false)
-        }
+        if (!data.countryCode)
+          throw new Error('Not a city. Click Somewhere else...')
+        setCityName(data.city || data.locality || '')
+        setCountry(data.countryName)
+        setEmoji(convertToEmoji(data.countryCode))
+      } catch (error) {
+        setGeocodingError(error.message)
+      } finally {
+        setIsLoadingGeocoding(false)
       }
-      fetchCityData()
-      return () => controller.abort()
+    }
+    fetchCityData()
   }, [mapLat, mapLng])
 
-  if(isLoadingGeocoding) return <Spinner />
-  if(geocodingError) return <Message message={geocodingError} />
+  if (isLoadingGeocoding) return <Spinner />
+  if (geocodingError) return <Message message={geocodingError} />
 
   return (
     <form className={styles.form}>
@@ -78,7 +76,7 @@ function Form() {
           id="date"
           type="date"
           onChange={(e) => setDate(new Date(e.target.value))}
-          value={date.toISOString().split("T")[0]} 
+          value={date.toISOString().split('T')[0]}
         />
       </div>
 
@@ -96,7 +94,7 @@ function Form() {
         <BackButton />
       </div>
     </form>
-  );
+  )
 }
 
-export default Form;
+export default Form
