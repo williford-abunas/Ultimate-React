e085a590ddff608396a4eecee0905c85ec3d2ed6
williford-abunas <abunaswilliford@gmail.com>
2025-03-09 16:30:13 +1300

    install redux

--- 25-Redux-intro/src/store.js
+++ 25-Redux-intro/src/store.js
@@ -6,6 +6,17 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 
+    case 'account/deposit':
+      return { ...state, balance: state.balance + action.payload }
+    case 'account/withdraw':
+      return { ...state, balance: state.balance - action.payload }
+    case 'account/requestLoan':
+      if (state.loan > 0) return state
+      return { ...state, loan: action.payload }
+    case 'account/payLoan':
+      return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
+    default:
+      return state
   }
 }
+
