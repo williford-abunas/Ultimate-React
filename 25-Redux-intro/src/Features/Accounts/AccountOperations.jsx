3b06f23564ef800b788cad80efe7b5fee387a751
williford-abunas <abunaswilliford@gmail.com>
2025-03-13 03:16:46 +1300

    debug currency state

--- 25-Redux-intro/src/Features/Accounts/AccountOperations.jsx
+++ 25-Redux-intro/src/Features/Accounts/AccountOperations.jsx
@@ -17,7 +17,7 @@ function AccountOperations() {
     if (!depositAmount) return
     dispatch(deposit(depositAmount, currency))
     setDepositAmount('')
-    setCurrency('')
+    setCurrency('USD')
   }
 
   function handleWithdrawal() {
