66e0d0aae2c260cac5ec9c39d41db2d23f04769a
williford-abunas <abunaswilliford@gmail.com>
2025-03-19 03:15:24 +1300

    using @apply to group classnames

--- 26-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 26-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -44,13 +44,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -58,12 +58,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
         <div>
-          <input
+          <input className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-offset-2
+            focus:ring-yellow-400"
             type="checkbox"
             name="priority"
             id="priority"
@@ -75,7 +76,8 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
+          <button className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300
+          focus:ring-offset-2 disabled:cursor-not-allowed" disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
         </div>
       </Form>
     </div>
--- 26-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 26-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -14,7 +14,7 @@ function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-    <input placeholder="Search order #" value={query} onChange = {(e) => setQuery(e.target.value)}/>
+    <input className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50" placeholder="Search order #" value={query} onChange = {(e) => setQuery(e.target.value)}/>
     </form>
   )
 }
