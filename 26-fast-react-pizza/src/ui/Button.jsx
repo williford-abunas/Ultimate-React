0a561070bff22c8bcf60a3f6c7665b2f691a865d
williford-abunas <abunaswilliford@gmail.com>
2025-03-20 03:06:13 +1300

    style menu

--- 26-fast-react-pizza/src/ui/Button.jsx
+++ 26-fast-react-pizza/src/ui/Button.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
 
 // eslint-disable-next-line react/prop-types
-function Button({children, disabled, to}) {
-  const className = "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
-  if(to) return <Link to={to} className={className}>{children}</Link>
+function Button({children, disabled, to, type}) {
+
+  const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+  }
+
+  if(to) return <Link to={to} className={styles[type]}>{children}</Link>
+
   return (
-    <button disabled={disabled} className={className}>{children}</button>
+    <button disabled={disabled} className={styles[type]}>{children}</button>
   )
 }
 
