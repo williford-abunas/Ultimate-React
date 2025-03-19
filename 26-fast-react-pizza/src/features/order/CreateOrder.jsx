e9aa6ba21e4e98e6c63745226b8b917d975751aa
williford-abunas <abunaswilliford@gmail.com>
2025-03-20 02:11:14 +1300

    style loader

--- 26-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 26-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import {Form, redirect, useNavigation, useActionData} from "react-router-dom"
 import {createOrder} from "../../services/apiRestaurant"
+import Button from "../../ui/Button"
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -76,8 +77,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name='cart' value={JSON.stringify(cart)} />
-          <button className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300
-          focus:ring-offset-2 disabled:cursor-not-allowed" disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
+          <Button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</Button>
         </div>
       </Form>
     </div>
--- 26-fast-react-pizza/src/ui/AppLayout.jsx
+++ 26-fast-react-pizza/src/ui/AppLayout.jsx
@@ -10,6 +10,7 @@ function AppLayout() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] h-screen">
       {isLoading && <Loader />}
+
       <Header />
       <div className="overflow-scroll">
         <main className="max-w-3xl mx-auto">
