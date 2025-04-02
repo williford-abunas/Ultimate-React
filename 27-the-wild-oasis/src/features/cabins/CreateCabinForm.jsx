541a537493232a27b36bd83799259cd0a8b82223
williford-abunas <abunaswilliford@gmail.com>
2025-04-02 18:30:17 +1300

    AddCabin modal createportal

--- 27-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 27-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import FormRow from '../../ui/FormRow'
 import { useCreateCabin } from './useCreateCabin'
 import { useEditCabin } from './useEditCabin'
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { id: editId, name, max_capacity, regular_price, ...rest } = cabinToEdit
 
   const editValues = {
@@ -40,6 +40,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         {
           onSuccess: () => {
             reset()
+            onCloseModal?.()
           },
         }
       )
@@ -49,6 +50,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         {
           onSuccess: () => {
             reset()
+            onCloseModal?.()
           },
         }
       )
@@ -59,7 +61,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? 'modal' : 'regular'}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -143,7 +148,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
--- 27-the-wild-oasis/src/ui/Modal.jsx
+++ 27-the-wild-oasis/src/ui/Modal.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+/* eslint-disable react/prop-types */
+import { createPortal } from 'react-dom'
+import { HiXMark } from 'react-icons/hi2'
+import styled from 'styled-components'
 
 const StyledModal = styled.div`
   position: fixed;
@@ -10,7 +13,7 @@ const StyledModal = styled.div`
   box-shadow: var(--shadow-lg);
   padding: 3.2rem 4rem;
   transition: all 0.5s;
-`;
+`
 
 const Overlay = styled.div`
   position: fixed;
@@ -22,7 +25,7 @@ const Overlay = styled.div`
   backdrop-filter: blur(4px);
   z-index: 1000;
   transition: all 0.5s;
-`;
+`
 
 const Button = styled.button`
   background: none;
@@ -47,4 +50,20 @@ const Button = styled.button`
     stroke: var(--color-grey-500); */
     color: var(--color-grey-500);
   }
-`;
+`
+
+function Modal({ children, onClose }) {
+  return createPortal(
+    <Overlay>
+      <StyledModal>
+        <Button onClick={onClose}>
+          <HiXMark />
+        </Button>
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  )
+}
+
+export default Modal
