9f67bc5f69bb2a0c47e685133a55ba59ffe4ddb5
williford-abunas <abunaswilliford@gmail.com>
2025-04-05 16:28:09 +1300

    reusable discount filter button

--- 27-the-wild-oasis/src/pages/Cabins.jsx
+++ 27-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,14 +1,15 @@
 import CabinTable from '../features/cabins/CabinTable'
+import AddCabin from '../features/cabins/AddCabin'
+import CabinTableOperations from '../features/cabins/CabinTableOperations'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
-import AddCabin from '../features/cabins/AddCabin'
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
