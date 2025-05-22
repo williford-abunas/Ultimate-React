4ff8ea89253bb9bf444b191541d61c3443ba083c
williford-abunas <abunaswilliford@gmail.com>
2025-05-23 11:48:52 +1200

    hydrate server side react components to client

--- 28-manual-ssr/client.js
+++ 28-manual-ssr/client.js
@@ -1,31 +1,4 @@
-const { readFileSync } = require('fs')
-const { createServer } = require('http')
-const { parse } = require('url')
-const { renderToString } = require('react-dom/server')
-const React = require('react')
-
-const htmlTemplate = readFileSync(`${__dirname}/index.html`, 'utf-8')
-const clientJS = readFileSync(`${__dirname}/client.js`, 'utf-8')
-
-const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).pathname
-
-  if (pathName === '/') {
-    const renderedHTML = renderToString(<Home />)
-    const html = htmlTemplate.replace('%%%CONTENT%%%', renderedHTML)
-    res.writeHead(200, { 'Content-type': 'text/html' })
-    res.end(html)
-  } else if (pathName === '/client.js') {
-    res.writeHead(200, { 'Content-type': 'application/javascript' })
-    res.end(clientJS)
-  } else {
-    res.end("URL can't be found")
-  }
-})
-
-server.listen(8000, () => {
-  console.log('Listening on port 8000')
-})
+ReactDOM.hydrateRoot(document.getElementById('root'), <Home />)
 
 const pizzas = [
   {
