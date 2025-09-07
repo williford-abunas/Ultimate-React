2b98b63fc652ee7342f36086c41668744aa632ca
williford-abunas <abunaswilliford@gmail.com>
2025-09-07 15:17:55 +1200

    added env variables for prod

--- 27-the-wild-oasis/src/supabase.js
+++ 27-the-wild-oasis/src/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = 'https://ayxacsottclfxlwekkzh.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5eGFjc290dGNsZnhsd2Vra3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMTk0NTgsImV4cCI6MjA1NzU5NTQ1OH0.-43KCSCAmoDcnoZhcJkiMy3KlgKHkeYB5DXm7srfbf0"
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
